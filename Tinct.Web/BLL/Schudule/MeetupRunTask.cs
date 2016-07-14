f70b1479f370f241676f70f0fb534af44fe77ae7
552448713 <552448713@qq.com>
2016-07-14 14:45:42 +0800

    fix some bug

---
+++ Tinct.Web/BLL/Schudule/MeetupRunTask.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tinct.Web.BLL.Schudule
+{
+    public class MeetupRunTask
+    {
+    }
+}
---
+++ Tinct.Web/Models/PlatformScheduleSetting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tinct.Web.Models
+{
+    public class PlatformScheduleSetting
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string PlatformName { get; set; }
+        public DateTime? LastRunTime { get; set; }
+        public DateTime? NextRunTime { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public DateTime? ExactRunTime { get; set; }
+        public int Interval { get; set; }
+
+        public event EventHandler<PlatformEventArgs> RunTaskEvent;
+        public void RaiseRunTaskEvent()
+        {
+            if (RunTaskEvent != null)
+            {
+                RunTaskEvent(this, null);
+            }
+        }
+    }
+}
